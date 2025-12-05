63d74dd37652dc354aa7e290dbfa72ae3a817c34
Donald-Berthelsen <djb230003@utdallas.edu>
2025-12-04 20:42:47 -0600

    Initial attempt at reading blocks

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
+storedBlocks = [None] * 3
+
+def get_block(workingFile, blockNumber, memoryDest):
+    workingFile.seek(blockNumber * 512)
+    storedBlocks[memoryDest] = workingFile.read(512)
+
 def print_file(filename):
-    workingFile = open(filename, "r")
+    workingFile = open(filename, "rb")
+
+    get_block(workingFile, 0, 0)
 
     workingFile.close()
 
