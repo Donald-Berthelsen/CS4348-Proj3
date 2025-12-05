79f13209266329d80d192924e63aa6919f98ba6d
Donald-Berthelsen <djb230003@utdallas.edu>
2025-12-04 22:12:09 -0600

    Implemented get_field
    Add function to retrieve specific field from block data

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
+import sys
+
 offset = 2  #How far the data is from the start of the file
 storedBlocks = [None] * 3
+
+def get_field(workingBlock, index):
+    index = index * 8
+    if index > 512 or index < 0:
+        print("ERROR: invalid field index")
+        sys.exit()
+
+    return int.from_bytes(workingBlock[index: index + 8].replace(b" ", b""), byteorder = 'big')
 
 def get_block(workingFile, blockNumber, memoryDest):
     workingFile.seek(blockNumber * 512 + offset)
@@ -19,7 +29,7 @@ print_file("test.idx")
 
 print(storedBlocks[0])
 print("------------------------------------")
-print(int.from_bytes(storedBlocks[0][16:24].replace(b" ", b""), byteorder = 'big'))
+print(get_field(storedBlocks[0], 2))
 print("------------------------------------")
 print(storedBlocks[1])
 print("------------------------------------")
