32e5fda9d50430fb3901d120172b09b977a297d4
Donald-Berthelsen <djb230003@utdallas.edu>
2025-12-04 23:38:50 -0600

    implemented extract

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import sys
 
 offset = 2  #How far the data is from the start of the file
@@ -31,7 +32,6 @@ def print_file(filename):
         workingFile.close()
         sys.exit()
 
-    print("key,value")
     totalBlocks = get_field(0, 2)
     for i in range(1, totalBlocks):
         get_block(workingFile, i, 0)
@@ -41,15 +41,51 @@ def print_file(filename):
 
     workingFile.close()
 
+def extract_file(sourceFileName, destFileName):
+    try:
+        sourceFile = open(sourceFileName, "rb")
+    except FileNotFoundError:
+        print("ERROR: file not found")
+        sys.exit()
+
+    get_block(sourceFile, 0, 0)
+    if(get_field(0, 0) != magicNumber):
+        print("ERROR: improper source file format")
+        sourceFile.close()
+        sys.exit()
+
+    if os.path.exists(destFileName):
+        print("ERROR: destination file already exists")
+        sys.exit()
+
+    destFile = open(destFileName, "w")
+
+    totalBlocks = get_field(0, 2)
+    for i in range(1, totalBlocks):
+        get_block(sourceFile, i, 0)
+        numKeys = get_field(0, 2)
+        for k in range(1, numKeys + 1):
+            destFile.write(f"{get_field(0, k + 2)},{get_field(0, k + 19 + 2)}\n")
+
+    sourceFile.close()
+    destFile.close()
+
 if len(sys.argv) < 2:
     print("Error: no arguments given")
     sys.exit()
 
 task = sys.argv[1]
-if(task == "print"):
-    if sys.argv[2]:
+if task == "print":
+    if len(sys.argv) > 2:
         print_file(sys.argv[2])
     else:
         print("ERROR: no file given")
+elif task == "extract":
+    if len(sys.argv) < 3:
+        print("ERROR: missing source file")
+    elif len(sys.argv) < 4:
+        print("ERROR: missing destination file")
+    else:
+        extract_file(sys.argv[2], sys.argv[3])
 else:
     print("ERROR: incrorrect command")
