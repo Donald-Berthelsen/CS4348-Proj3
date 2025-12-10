958b9978f16a1b4673f7c7d59d91957a2d062c38
Donald-Berthelsen <djb230003@utdallas.edu>
2025-12-10 14:58:06 -0600

    Implemented print_block

--- main.py
+++ main.py
@@ -39,6 +39,24 @@ def get_block(workingFile, blockNumber, memoryDest):
 
     storedBlocks[memoryDest] = workingFile.read(512)
 
+def print_block(memoryDest):
+    print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+    print(f"Block ID: {get_field(memoryDest, 0)}")
+    print(f"Block Parent ID: {get_field(memoryDest, 1)}")
+    print(f"Block Size: {get_field(memoryDest, 2)}")
+    print("Block Keys:\t", end = '')
+    for i in range(0, 18):
+        print(f"{get_field(memoryDest, 3 + i)}", end = ',')
+    print(f"{get_field(memoryDest, 3 + 18)}")
+    print("Block Values:\t", end = '')
+    for i in range(0, 18):
+        print(f"{get_field(memoryDest, 3 + 19 + i)}", end = ',')
+    print(f"{get_field(memoryDest, 3 + 18 + i)}")
+    print("Block Pointers:\t", end = '')
+    for i in range(0, 19):
+        print(f"{get_field(memoryDest, 3 + 19 + 19 + i)}", end = ',')
+    print(f"{get_field(memoryDest, 3 + 19 + 19)}")
+
 def set_block(filename, blockNumber):
     workingFile = open(filename, "wb")
 
@@ -59,6 +77,42 @@ def create_file(filename):
 
     set_block(filename, 0)
 
+def search_file(filename, val):
+    try:
+        workingFile = open(filename, "rb")
+    except FileNotFoundError:
+        print("ERROR: file not found")
+        sys.exit()
+
+    get_block(workingFile, 0, 0)
+    nextBlock = get_field(0, 1)
+
+    while nextBlock != 0:
+        print_block(0)
+
+        get_block(workingFile, nextBlock, 0)
+        blockSize = get_field(0, 2)
+
+        if val < get_field(0, 3):
+            nextBlock = get_field(0, 3 + 19 + 19)
+            continue
+
+        if val > get_field(0, 2 + blockSize):
+            nextBlock = get_field(0, 3 + 19 + 19 + blockSize)
+            continue
+
+        for i in range(0, blockSize):
+            workingKey = get_field(0, i + 3)
+            if workingKey == val:
+                print(f"{get_field(0, i + 3)},{get_field(0, i + 19 + 3)}")
+                return
+            
+            if i < blockSize:
+                if val > workingKey and val < get_field(0, i + 1 + 3):
+                    nextBlock = get_field(0, 3 + 19 + 19 + i + 1)
+                    break
+
+    print(f"ERROR: unable to find index {val} in file {filename}")
 
 def print_file(filename):
     try:
@@ -121,6 +175,17 @@ if task == "create":
         print("ERROR: missing file name")
     else:
         create_file(sys.argv[2])
+if task == "search":
+    if len(sys.argv) < 3:
+        print("ERROR: no file given")
+    elif len(sys.argv) < 4:
+        print("ERROR: no index given")
+    elif not sys.argv[3].isdigit():
+        print("ERROR: index must be unsigned integer")
+    else:
+        if int(sys.argv[3]) < 0:
+            print("ERROR: index must be unsigned integer")
+        search_file(sys.argv[2], int(sys.argv[3]))
 elif task == "print":
     if len(sys.argv) > 2:
         print_file(sys.argv[2])
